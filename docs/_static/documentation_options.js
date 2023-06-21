c4ae9a5437878704ab3966e111941a7b9a7abc6d
gagolews <m.gagolewski@gmail.com>
2023-06-21 21:15:09 +1000

    more

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.9.0.9003 (draft)',
+    VERSION: 'v0.9.0.9004 (draft)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
